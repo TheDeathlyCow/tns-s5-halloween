c694977a8f6c6eaac3e1c0828f0c85682bc6f184
TheDeathlyCow <thedeathlycow@gmail.com>
2020-10-03 02:25:19 +1300

    new start checker gen code

--- data/halloween-bosses/functions/crimson_forest/gen_start_checker.py
+++ data/halloween-bosses/functions/crimson_forest/gen_start_checker.py
@@ -3,45 +3,38 @@ This module is used to generate the dispenser checks for the direction and time 
 It is mainly to have the execute if for each of the 9 slots in a dispenser.
 """
 
-DIR_TEMPLATE_FRONT = """execute as @e[type=minecraft:armor_stand,tag=dir_dispenser] at @s if block ~ ~ ~ minecraft:dropper[facing=east]{Items:[{Slot:"""
-DIRECTION_TEMPLATE_BACK = """b,id:"minecraft:compass",Count:1b,tag:{display:{Name:'{"text":"Direction","color":"aqua","bold":true,"italic":false}'},direction_compass:1b,LodestoneDimension:"minecraft:the_nether",LodestoneTracked:1b}}]} run function halloween-bosses:crimson_forest/accept_token"""
+DIR_TYPE = 'dir'
+TIME_TYPE = 'time'
 
-TIME_TEMPLATE_FRONT = """execute as @e[type=minecraft:armor_stand,tag=time_dispenser] at @s if block ~ ~ ~ minecraft:dropper[facing=east]{Items:[{Slot:"""
-TIME_TEMPLATE_BACK = """b,id:"minecraft:clock",Count:1b,tag:{display:{Name:'{"text":"Time","color":"gold","bold":true,"italic":false}'},time_clock:1b}}]} run function halloween-bosses:crimson_forest/accept_token"""
+COMPASS_ITEM = """id:"minecraft:compass",Count:1b,tag:{display:{Name:'{"text":"Direction","color":"aqua","bold":true,"italic":false}'},direction_compass:1b,LodestoneDimension:"minecraft:the_nether",LodestoneTracked:1b}"""
+CLOCK_ITEM = """id:"minecraft:clock",Count:1b,tag:{display:{Name:'{"text":"Time","color":"gold","bold":true,"italic":false}'},time_clock:1b}"""
+
+TEMPLATE = """execute as @e[type=minecraft:armor_stand,tag={TYPE}_dispenser] at @s if block ~ ~ ~ {BLOCK}[facing={FACING}]{Items:[{Slot:{SLOT}b,{ITEM}}]} run function halloween-bosses:crimson_forest/accept_tokens/{TYPE}/slot_{SLOT}"""
 
 TOP_COMMENT = '# this file was generated with gen_start_checker.py\n'
 
-def generate_direction():
-    global TEMPLATE_FRONT
-    global DIRECTION_TEMPLATE_BACK
-    
 
-    outfile = open('direction_checker.mcfunction', 'w')
+def gen_file(inType, item, filename, num_slots=9, block='minecraft:dropper', facing='east'):
+    """
+    Generates an mcfunction file for detecting an item in a container. 
+    The default container type is a dropper.
+    """
+    outfile = open(filename, 'w')
     outfile.write(TOP_COMMENT)
 
-    for i in range(0, 9):
-        line = DIR_TEMPLATE_FRONT + str(i) + DIRECTION_TEMPLATE_BACK + '\n'
-        print(line)
+    for slot in range(0, num_slots):
+        line = TEMPLATE.replace('{TYPE}', inType)
+        line = line.replace('{BLOCK}', block)
+        line = line.replace('{FACING}', facing)
+        line = line.replace('{ITEM}', item)
+        line = line.replace('{SLOT}', str(slot))
+        line += '\n'
+
         outfile.write(line)
     
     outfile.close()
-
-def generate_time():
-    global TEMPLATE_FRONT
-    global TIME_TEMPLATE_BACK
-
-    outfile = open('time_checker.mcfunction', 'w')
-    outfile.write(TOP_COMMENT)
-
-    for i in range(0, 9):
-        line = TIME_TEMPLATE_FRONT + str(i) + TIME_TEMPLATE_BACK + '\n'
-        print(line)
-        outfile.write(line)
-
-    outfile.close()
-
 
 if __name__ == '__main__':
-    generate_direction()
-    generate_time()
+    gen_file(DIR_TYPE, COMPASS_ITEM, 'direction_checker.mcfunction')
+    gen_file(TIME_TYPE, CLOCK_ITEM, 'time_checker.mcfunction')
 
